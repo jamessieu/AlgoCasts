e5206f5860321368aea37479b16d36cbe0abe609
James Sieu <jamessieu@Jamess-MacBook-Pro.local>
2018-11-12 15:15:43 -0800

    completed reverseInt and maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  //loop through string
+  //store letter in hash table (object), letter is key and count is value
+  //return letter with the most count
+
+  const myStrObj = {};
+  let maxCount = 0;
+  let char;
+
+  for (character of str) {
+    if (!myStrObj[character]) {
+      myStrObj[character] = 1;
+    } else {
+      myStrObj[character] += 1;
+    }
+  }
+
+  for (key in myStrObj) {
+    if (myStrObj[key] > maxCount) {
+      char = key;
+      maxCount = myStrObj[key];
+    }
+  }
+  return char;
+}
 
 module.exports = maxChar;
