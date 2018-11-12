c257f760893ab8719b44ef790e38c14324e57195
James Sieu <jamessieu@gmail.com>
2018-11-12 08:44:25 -0800

    initial commit of solutions

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, index) => {
+    return char === str[str.length - 1 - index];
+  });
+}
+
+// function palindrome(str) {
+//   const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//   return reversed === str;
+// }
 
 module.exports = palindrome;
