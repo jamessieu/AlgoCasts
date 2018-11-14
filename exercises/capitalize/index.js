d937191d315399cf41d5aebb8fc297f78ee6b07e
James Sieu <jamessieu@Jamess-MacBook-Pro.local>
2018-11-13 19:33:31 -0800

    capitalize complete

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,13 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const result = [];
+
+  for (let word of str.split(' ')) {
+    result.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return result.join(' ');
+}
 
 module.exports = capitalize;
